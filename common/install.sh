e223cb1244e46c5457b1d0a88cf75e708adc1027
Gabriel Howard <j0s3ghoward@gmail.com>
2019-01-18 20:09:29 +0100

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -7,5 +7,6 @@ chmod 660 $WELLBEING_PREF_FILE
 WELLBEING_PREF_FOLDER=/data/data/com.google.android.apps.wellbeing/shared_prefs/
 mkdir -p $WELLBEING_PREF_FOLDER
 cp -p $WELLBEING_PREF_FILE $WELLBEING_PREF_FOLDER
-  am force-stop "com.google.android.apps.wellbeing"
+
+am force-stop "com.google.android.apps.wellbeing"
 fi
